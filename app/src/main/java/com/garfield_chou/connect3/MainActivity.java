5879c51d71d35e65be03c620d117deff331ac501
shaudai <shaudai0402@hotmail.com>
2016-12-20 23:05:13 +0800

    Add activePlayer to change between yellow and red

--- app/src/main/java/com/garfield_chou/connect3/MainActivity.java
+++ app/src/main/java/com/garfield_chou/connect3/MainActivity.java
@@ -6,11 +6,20 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // 0 = yellow, 1 = red
+    int activePlayer = 0;
 
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
         counter.setTranslationY(-1500f);
-        counter.setImageResource(R.drawable.yellow);
+
+        if (0 == activePlayer) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
         counter.animate().translationYBy(1500f).rotation(3600f).setDuration(300);
     }
 
