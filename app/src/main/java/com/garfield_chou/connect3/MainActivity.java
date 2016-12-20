439fe154602f0f0e3ab91c8db5b7862dca00f54c
shaudai <shaudai0402@hotmail.com>
2016-12-20 23:32:59 +0800

    Add tag and gameState[]

--- app/src/main/java/com/garfield_chou/connect3/MainActivity.java
+++ app/src/main/java/com/garfield_chou/connect3/MainActivity.java
@@ -9,18 +9,29 @@ public class MainActivity extends AppCompatActivity {
     // 0 = yellow, 1 = red
     int activePlayer = 0;
 
+    // 2 means unplayed
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
-        counter.setTranslationY(-1500f);
+        System.out.println(counter.getTag().toString());
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (0 == activePlayer) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
-        } else {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
+        if (2 == gameState[tappedCounter]) {
+
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1500f);
+
+            if (0 == activePlayer) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500f).rotation(3600f).setDuration(300);
         }
-        counter.animate().translationYBy(1500f).rotation(3600f).setDuration(300);
     }
 
     @Override
