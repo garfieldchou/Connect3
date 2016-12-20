946b903e78947c8c4c40edb294fd341ea9c1ee4f
garfield_chou <garfield_chou@asus.com>
2016-12-20 15:27:37 +0800

    Drop in yellow in the middle

--- app/src/main/java/com/garfield_chou/connect3/MainActivity.java
+++ app/src/main/java/com/garfield_chou/connect3/MainActivity.java
@@ -2,8 +2,17 @@ package com.garfield_chou.connect3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn (View view) {
+        ImageView counter = (ImageView) view;
+        counter.setTranslationY(-1500f);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500f).rotation(3600f).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
