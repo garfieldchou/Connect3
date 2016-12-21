89e39b13d57c954fe9ca6b11824f51e6ebfebdac
garfield_chou <garfield_chou@asus.com>
2016-12-21 09:44:23 +0800

    Add winningPositions for determining the winner

--- app/src/main/java/com/garfield_chou/connect3/MainActivity.java
+++ app/src/main/java/com/garfield_chou/connect3/MainActivity.java
@@ -11,10 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     // 2 means unplayed
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
-        System.out.println(counter.getTag().toString());
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
         if (2 == gameState[tappedCounter]) {
@@ -31,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
                 activePlayer = 0;
             }
             counter.animate().translationYBy(1500f).rotation(3600f).setDuration(300);
+        }
+
+        for (int[] winningPosition : winningPositions) {
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                    gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                    gameState[winningPosition[0]] != 2 ) {
+                System.out.println(gameState[winningPosition[0]]);
+            }
         }
     }
 
